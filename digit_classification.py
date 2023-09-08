78c1f842c4f200e132fc6f66687c58b75b060e09
bestdeve <kumar.roushan00@gmail.com>
2023-09-08 17:58:38 +0530

    Chnages for assgnment3

--- digit_classification.py
+++ digit_classification.py
@@ -1,81 +1,46 @@
-# Standard scientific Python imports
+import itertools
 import matplotlib.pyplot as plt
-
-# Import datasets, classifiers, and performance metrics
 from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
+from utils import hyperparameter_tuning, prepare_data_splits
+
+def main():
 
 
-# Function to split data into training, development, and test sets
-def split_train_dev_test(X, y, test_size, dev_size):
-    # First, split the data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    # Load the digits dataset
+    digits = datasets.load_digits()
 
-    # Next, split the test set into development and test sets
-    X_dev, X_test, y_dev, y_test = train_test_split(X_test, y_test, test_size=dev_size, random_state=42)
+    # Flatten the images
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+    X = data
+    y = digits.target
 
-    return X_train, y_train, X_dev, y_dev, X_test, y_test
+    # Define parameter ranges
+    gamma_ranges = [0.001, 0.01, 0.1, 1, 100]
+    C_ranges = [0.1, 1, 2, 5, 10]
+    list_of_all_param_combination = list(itertools.product(gamma_ranges, C_ranges))
+
+    # Define test and dev set sizes
+    test_size = [0.1, 0.2, 0.3]
+    dev_size = [0.1, 0.2, 0.3]
+    list_of_size = list(itertools.product(test_size, dev_size))
+
+    for test_frac, dev_frac in list_of_size:
+        # Split the data into train, dev, and test sets
+        X_train, Y_train, X_dev, Y_dev, X_test, Y_test = prepare_data_splits(X, y, test_frac, dev_frac)
+
+        # Tune hyperparameters
+        model, gamma, C, cur_metric, train_metric = hyperparameter_tuning(X_train, Y_train, X_dev, Y_dev, list_of_all_param_combination)
+
+        # Predict on the test set
+        pred_test_result = model.predict(X_test)
+        test_metrix = metrics.accuracy_score(y_pred=pred_test_result, y_true=Y_test)
+
+        # Print results
+        print(
+        f'Training Size:{1 - (test_frac + dev_frac)} Test Size:{test_frac} Dev Size:{dev_frac} Train Acc:{train_metric} Test Acc:{test_metrix} Val Acc:{cur_metric}')
+        print(f' SVM model Metrix with gamma:{gamma} and C:{C}')
 
 
-# Function to predict and evaluate a model
-def predict_and_eval(model, X_test, y_test):
-    # Predict the value of the digit on the test subset
-    predicted = model.predict(X_test)
-
-    # Print classification report
-    print(f"Classification report for classifier {model}:\n{metrics.classification_report(y_test, predicted)}\n")
-
-    # Display confusion matrix
-    disp = metrics.ConfusionMatrixDisplay.from_estimator(model, X_test, y_test)
-    disp.figure_.suptitle("Confusion Matrix")
-    print(f"Confusion matrix:\n{disp.confusion_matrix}\n")
-
-    # Rebuild classification report from confusion matrix
-    y_true = []
-    y_pred = []
-    cm = disp.confusion_matrix
-    for gt in range(len(cm)):
-        for pred in range(len(cm)):
-            y_true += [gt] * cm[gt][pred]
-            y_pred += [pred] * cm[gt][pred]
-
-    print("Classification report rebuilt from confusion matrix:\n")
-    print(metrics.classification_report(y_true, y_pred))
-
-
-# Load the digits dataset
-digits = datasets.load_digits()
-
-# Visualize a subset of the data
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
-
-# Flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
-
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
-
-# Split data into training, development, and test sets
-X_train, y_train, X_dev, y_dev, X_test, y_test = split_train_dev_test(data, digits.target, test_size=0.2, dev_size=0.1)
-
-# Learn the digits on the training subset
-clf.fit(X_train, y_train)
-
-# Predict and evaluate the model on the test subset
-predict_and_eval(clf, X_test, y_test)
-
-# Visualize the first 4 test samples and their predicted values
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-predicted = clf.predict(X_test)  # Move this line here to define 'predicted'
-for ax, image, prediction in zip(axes, X_test[:4], predicted[:4]):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
-
-plt.show()
+if __name__ == '__main__':
+    main()
